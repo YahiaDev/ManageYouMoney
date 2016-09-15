9ac64b433ba5acfb6aa19743bf4ec5bc512c3cb3
yahia ammar <yahiaammar@macbook-air-de-yahia.home>
2016-09-15 23:19:14 +0200

    connect spring rest with angular

--- MyAngular1XApp/modules/authentication/authenticationService.js
+++ MyAngular1XApp/modules/authentication/authenticationService.js
@@ -1,6 +1,6 @@
 'use strict';
 var module = angular.module('authentication');
-module.factory('AuthService',['$rootScope', '$q', 'Session', '$cookieStore', function($rootScope, $q, Session, $cookieStore){
+module.factory('AuthService',['$rootScope', '$q', 'Session', '$cookieStore', '$http', 'AUTH_EVENTS', '$state',function($rootScope, $q, Session, $cookieStore, $http, AUTH_EVENTS){
     var service = {};
     service.authenticate = function(user){
         var deffered = $q.defer();
@@ -12,6 +12,17 @@ module.factory('AuthService',['$rootScope', '$q', 'Session', '$cookieStore', fun
         $cookieStore.put('logedUser', user);
         Session.create(user.sessionId, user.id , user.role);
         console.info(user.login + ' , ' + user.password);
+
+        $http.get('http://localhost:8080/manageyourmoney/getUser').then(function successCallback(response){
+          $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+          console.log('success callback');
+          $state.go('home');
+        }, function errorCallback(response){
+            $rootScope.$broadcast(AUTH_EVENTS.loginFailed);  
+            console.log('error callback');
+
+        });
+
         return deffered.promise;
 
     };
--- manageyourmoney/src/main/java/com/manageyourmoney/controller/UserController.java
+++ manageyourmoney/src/main/java/com/manageyourmoney/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.manageyourmoney.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,14 @@ import com.manageyourmoney.component.User;
 
 @RestController
 public class UserController {
+	@Autowired
+	private User user;
 	@RequestMapping(value="getUser", method= RequestMethod.GET,headers="Accept=application/json")
 	public User getUser() {
-		User user = new User(12.0, "test", "1254", "yahia", "ammar");
+		//User user = new User(12.0, "test", "1254", "yahia", "ammar");
+		user.setId(11);
+		user.setName("yahia");
+		user.setLogin("yahia.ammar.info@gmail.com");
 		return user;
 	}
 }
